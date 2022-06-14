b90251e341c25d0690efbc31b35a2fbf9033849c
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2022-06-14 16:13:04 +0200

    Added unit tests forr enum extensions

---
+++ tests/UnitTests/Library/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using System.ComponentModel.DataAnnotations;
+using Library.Attributes;
+using Library.Extensions;
+
+namespace UnitTests.Library.Extensions
+{
+    public class EnumExtensionsTests
+    {
+        public enum EnumType
+        {
+            [Display(Name = "Name")]
+            [Symbol("s")]
+            EnumValue1,
+
+            EnumValue2,
+        }
+
+        [Fact]
+        public void GetDisplayName_ShouldReturnDisplayName_WhenEnumHasAttribute()
+        {
+            var result = EnumType.EnumValue1.GetDisplayName();
+            result.Should().Be("Name");
+        }
+
+        [Fact]
+        public void GetDisplayName_ShouldReturnNull_WhenEnumDoesntHaveAttribute()
+        {
+            var result = EnumType.EnumValue2.GetDisplayName();
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetSymbol_ShouldReturnSymbol_WhenEnumHasAttribute()
+        {
+            var result = EnumType.EnumValue1.GetSymbol();
+            result.Should().Be("s");
+        }
+
+        [Fact]
+        public void GetSymbol_ShouldReturnNull_WhenEnumDoesntHaveAttribute()
+        {
+            var result = EnumType.EnumValue2.GetSymbol();
+            result.Should().BeNull();
+        }
+    }
+}
