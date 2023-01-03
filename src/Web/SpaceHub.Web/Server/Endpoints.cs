0c2579b745c2c50aeb5ae46015e8e957422c00ee
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2023-01-03 16:22:28 +0100

    Switched to minimal endpoints

---
+++ src/Web/SpaceHub.Web/Server/Endpoints.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using SpaceHub.Application.Features.Launches;
+using SpaceHub.Application.Features.News;
+using SpaceHub.Contracts.Enums;
+
+namespace SpaceHub.Web.Server;
+
+public static class EndpointsExtension
+{
+    public static void AddEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/api/articles", async (IMediator mediator, string searchValue, int pageNumber, int itemsPerPage) =>
+        {
+            return await mediator.Send(new GetNewsQuery(searchValue, pageNumber, itemsPerPage));
+        });
+
+        app.MapGet("/api/launches", async (IMediator mediator, ETimeFrame timeFrame, string searchValue, int pageNumber, int itemsPerPage) =>
+        {
+            return await mediator.Send(new GetLaunchesQuery(timeFrame, searchValue, pageNumber, itemsPerPage));
+        });
+
+        app.MapGet("/api/launches/{id}", async (IMediator mediator, string id) =>
+        {
+            return await mediator.Send(new GetLaunchDetailsQuery(id));
+        });
+    }
+}
--- src/Web/SpaceHub.Web/Server/Program.cs
+++ src/Web/SpaceHub.Web/Server/Program.cs
@@ -1,5 +1,6 @@
 using SpaceHub.Application;
 using SpaceHub.Infrastructure;
+using SpaceHub.Web.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +22,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.AddEndpoints();
 
 app.UseHttpsRedirection();
 app.UseBlazorFrameworkFiles();
