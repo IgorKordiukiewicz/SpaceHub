4490218ebe1374ff391a329fa92b17b3a9d652cf
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2023-01-11 23:52:11 +0100

    Moved data synchronization code to handlers

---
+++ src/Application/Features/Launches/Update.cs
@@ -0,0 +1,143 @@
+﻿using MediatR;
+using MongoDB.Driver;
+using SpaceHub.Infrastructure.Api.Responses;
+using SpaceHub.Infrastructure.Api;
+using SpaceHub.Infrastructure.Data.Models;
+using SpaceHub.Infrastructure.Enums;
+using SpaceHub.Infrastructure.Data;
+using MongoDB.Driver.Linq;
+using System.Globalization;
+
+namespace SpaceHub.Application.Features.Launches;
+
+public record UpdateLaunchesCommand() : IRequest;
+
+internal class UpdateLaunchesHandler : IRequestHandler<UpdateLaunchesCommand>
+{
+    private readonly DbContext _db;
+    private readonly ILaunchApi _api;
+
+    public UpdateLaunchesHandler(DbContext db, ILaunchApi api)
+    {
+        _db = db;
+        _api = api;
+    }
+
+    public async Task<Unit> Handle(UpdateLaunchesCommand request, CancellationToken cancellationToken)
+    {
+        var launches = await GetLaunchesFromApi();
+        var existingIds = _db.Launches.AsQueryable().Select(x => x.ApiId).ToHashSet();
+
+        var writes = new List<WriteModel<LaunchModel>>();
+        foreach (var launch in launches)
+        {
+            if (existingIds.Contains(launch.Id))
+            {
+                writes.Add(CreateUpdateModel(launch));
+            }
+            else
+            {
+                writes.Add(CreateInsertModel(launch));
+            }
+        }
+
+        var _ = await _db.Launches.BulkWriteAsync(writes);
+
+        await _db.CollectionsLastUpdates.UpdateOneAsync(
+            x => x.CollectionType == ECollection.Launches,
+            Builders<CollectionLastUpdateModel>.Update.Set(x => x.LastUpdate, DateTime.UtcNow));
+
+        return Unit.Value;
+    }
+
+    private async Task<List<LaunchDetailResponse>> GetLaunchesFromApi()
+    {
+        var lastUpdateTime = await _db.CollectionsLastUpdates.AsQueryable()
+            .Where(x => x.CollectionType == ECollection.Launches)
+            .Select(x => x.LastUpdate)
+            .SingleAsync();
+
+        var launches = new List<LaunchDetailResponse>();
+        int offset = 0;
+        // TODO: Inefficient, maybe break when Launches.Count < 100 (limit) to avoid having to call api one more time
+        while (true)
+        {
+            var result = await _api.GetLaunchesUpdatedBetweenAsync(lastUpdateTime.ToQueryParameter(), DateTime.UtcNow.ToQueryParameter(), offset);
+            if (!result.Launches.Any())
+            {
+                break;
+            }
+
+            launches.AddRange(result.Launches);
+            offset += 100;
+        }
+
+        return launches;
+    }
+
+    private UpdateOneModel<LaunchModel> CreateUpdateModel(LaunchDetailResponse response)
+    {
+        var filter = Builders<LaunchModel>.Filter.Eq(x => x.ApiId, response.Id);
+        var update = Builders<LaunchModel>.Update
+            .Set(x => x.Status, response.Status.Name)
+            .Set(x => x.Date, response.Date)
+            .Set(x => x.WindowStart, response.WindowStart)
+            .Set(x => x.WindowEnd, response.WindowEnd);
+        return new UpdateOneModel<LaunchModel>(filter, update);
+    }
+
+    private InsertOneModel<LaunchModel> CreateInsertModel(LaunchDetailResponse response)
+    {
+        LaunchMissionModel? mission = null;
+        if (response.Mission is not null)
+        {
+            mission = new()
+            {
+                Name = response.Mission.Name,
+                Type = response.Mission.Type,
+                Description = response.Mission.Description,
+                OrbitName = response.Mission.Orbit?.Name ?? ""
+            };
+        }
+
+        var videos = new List<LaunchVideoModel>();
+        if (response.Videos is not null)
+        {
+            foreach (var video in response.Videos)
+            {
+                videos.Add(new()
+                {
+                    Url = video.Url,
+                    Title = video.Title,
+                });
+            }
+        }
+
+        return new InsertOneModel<LaunchModel>(new LaunchModel()
+        {
+            ApiId = response.Id,
+            Name = response.Name,
+            Status = response.Status.Name,
+            Date = response.Date,
+            WindowStart = response.WindowStart,
+            WindowEnd = response.WindowEnd,
+            ImageUrl = response.ImageUrl,
+            Mission = mission,
+            Pad = new()
+            {
+                Name = response.Pad.Name,
+                LocationName = response.Pad.Location.Name,
+                CountryCode = response.Pad.Location.CountryCode,
+                Latitude = double.Parse(response.Pad.Latitude, CultureInfo.InvariantCulture),
+                Longitude = double.Parse(response.Pad.Longitude, CultureInfo.InvariantCulture),
+                WikiUrl = response.Pad.WikiUrl ?? "",
+                MapImageUrl = response.Pad.Location.MapImageUrl,
+                MapUrl = response.Pad.MapUrl ?? ""
+            },
+            Videos = videos,
+            AgencyName = response.Agency.Name,
+            AgencyApiId = response.Agency.Id,
+            RocketApiId = response.Rocket.Configuration.Id,
+        });
+    }
+}
---
+++ src/Application/Features/News/Update.cs
@@ -0,0 +1,72 @@
+﻿using MediatR;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using SpaceHub.Contracts.ViewModels;
+using SpaceHub.Infrastructure.Api;
+using SpaceHub.Infrastructure.Api.Responses;
+using SpaceHub.Infrastructure.Data;
+using SpaceHub.Infrastructure.Data.Models;
+using SpaceHub.Infrastructure.Enums;
+
+namespace SpaceHub.Application.Features.News;
+
+public record UpdateArticlesCommand() : IRequest;
+
+internal class UpdateArticlesHandler : IRequestHandler<UpdateArticlesCommand>
+{
+    private readonly DbContext _db;
+    private readonly IArticleApi _api;
+
+    public UpdateArticlesHandler(DbContext db, IArticleApi api)
+    {
+        _db = db;
+        _api = api;
+    }
+
+    public async Task<Unit> Handle(UpdateArticlesCommand request, CancellationToken cancellationToken)
+    {
+        var lastUpdateTime = await _db.CollectionsLastUpdates.AsQueryable()
+            .Where(x => x.CollectionType == ECollection.Articles)
+            .Select(x => x.LastUpdate)
+            .SingleAsync();
+
+        var now = DateTime.UtcNow; // TODO: Use some kind of interface for DateTime
+        var articles = await _api.GetArticlesPublishedBetweenAsync(lastUpdateTime.ToQueryParameter(), now.ToQueryParameter());
+        if (articles is null)
+        {
+            // TODO: log,
+            // method should return Result<T>, if this method's result is bad then maybe add background job in hangfire to retry in e.g. 30minutes
+            return Unit.Value;
+        }
+
+        if (articles.Any())
+        {
+            var newArticles = new List<ArticleModel>();
+            foreach (var article in articles)
+            {
+                newArticles.Add(CreateModel(article);
+            }
+
+            await _db.Articles.InsertManyAsync(newArticles);
+        }
+
+        await _db.CollectionsLastUpdates.UpdateOneAsync(
+            x => x.CollectionType == ECollection.Articles,
+            Builders<CollectionLastUpdateModel>.Update.Set(x => x.LastUpdate, now));
+
+        return Unit.Value;
+    }
+
+    private ArticleModel CreateModel(ArticleResponse response)
+    {
+        return new ArticleModel()
+        {
+            Title = response.Title,
+            Summary = response.Summary,
+            ImageUrl = response.ImageUrl,
+            NewsSite = response.NewsSite,
+            PublishDate = response.PublishDate,
+            Url = response.Url,
+        };
+    }
+}
